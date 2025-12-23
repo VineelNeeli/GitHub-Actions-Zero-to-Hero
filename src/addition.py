f784488eb053a0ec92a011100b1a45164f235bec
VineelNeeli <138756853+VineelNeeli@users.noreply.github.com>
2025-12-23 21:41:24 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -2,6 +2,7 @@
 # This is a test commit
 #This is my second test commit
 # this is my sec commit after enabling workflows
+#this is the commit after enabling self-hosted runners
 def add(a, b):
     return a + b
 
